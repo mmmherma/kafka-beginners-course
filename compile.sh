e0a46cfc25aae4ed9ffe9a8d49b6236d21e8fd91
mmmherma <marcosherma@gmail.com>
2020-03-26 13:39:58 +0100

    Fixed Dockerfiles due to refactoring

--- compile.sh
+++ compile.sh
@@ -12,6 +12,7 @@ error () {
 mvn package
 
 # Dockerize application
+cd kafka-basics
 docker build --no-cache -f docker/Dockerfile-producer-demo -t producer-demo:v1 .
 docker build --no-cache -f docker/Dockerfile-producer-demo-with-callbacks -t producer-demo-with-callbacks:v1 .
 docker build --no-cache -f docker/Dockerfile-producer-demo-keys -t producer-demo-keys:v1 .
@@ -19,6 +20,7 @@ docker build --no-cache -f docker/Dockerfile-consumer-demo -t consumer-demo:v1 .
 docker build --no-cache -f docker/Dockerfile-consumer-demo-groups -t consumer-demo-groups:v1 .
 docker build --no-cache -f docker/Dockerfile-consumer-demo-with-threads -t consumer-demo-with-threads:v1 .
 docker build --no-cache -f docker/Dockerfile-consumer-demo-assign-seek -t consumer-demo-assign-seek:v1 .
-docker build --no-cache -f docker/Dockerfile-twitter-producer -t twitter-producer:v1 .
 docker build --no-cache -f docker/Dockerfile-producer-demo-safe -t producer-demo-safe:v1 .
+cd ../kafka-producer-twitter
+docker build --no-cache -f docker/Dockerfile-twitter-producer -t twitter-producer:v1 .
 docker build --no-cache -f docker/Dockerfile-twitter-producer-high-throughput -t twitter-producer-high-throughput:v1 .
